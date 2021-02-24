0facc94f642164f802a6a42e1ed7ee1da6950f67
gavin33361 <gzheng0214@gmail.com>
2021-02-24 06:54:40 -0500

    Fixed socket connect link

--- client/src/components/Calculator.js
+++ client/src/components/Calculator.js
@@ -4,7 +4,7 @@ import Button from "./Button";
 import Display from "./Display";
 import Results from "./Results";
 
-const socket = io.connect("http://localhost:4000");
+const socket = io.connect("/");
 
 const Calculator = () => {
   const [expression, setExpression] = useState("");
